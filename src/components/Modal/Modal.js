b6741963ab77ef01884e7627b6ecfcc54c4ab1c2
mazen <mazenadel19@gmail.com>
2021-08-03 17:29:43 +0200

    animation timing

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -2,9 +2,18 @@ import React from 'react'
 import Transition from 'react-transition-group/Transition'
 import './Modal.css'
 
+const animationTiming = {
+	enter: 400,
+	exit: 1000,
+}
+
 const modal = props => {
 	return (
-		<Transition in={props.show} timeout={1000} mountOnEnter unmountOnExit>
+		<Transition
+			in={props.show}
+			timeout={animationTiming}
+			mountOnEnter
+			unmountOnExit>
 			{state => {
 				const cssClasses = `Modal ${
 					state === 'entering'
